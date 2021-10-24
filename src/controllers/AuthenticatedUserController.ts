ed6768190436f9304ac78c60c294e3fdab42c5c0
BrunoLucena99 <brunohenriquelucena@gmail.com>
2021-10-24 20:52:09 -0300

    feat: insert browser condition on web authorizate route

--- src/controllers/AuthenticatedUserController.ts
+++ src/controllers/AuthenticatedUserController.ts
@@ -3,13 +3,13 @@ import { AuthenticateUserService } from "../services/AuthenticateUserService"
 
 class AuthenticateUserController {
 	async handle(req: Request, res: Response) {
-		const { code } = req.body;
+		const { code, browser } = req.body;
 		const service = new AuthenticateUserService();
 		try {
-			const result = await service.execute(code); 
+			const result = await service.execute(code, browser); 
 			return res.json(result);
 		} catch (err) {
-			return res.json({ error: err.message });
+			return res.status(500).json({error: err.message});
 		}
 	}
 }
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -14,61 +14,70 @@ interface UserResponse {
 }
 
 class AuthenticateUserService {
-	async execute(code: string) {
+	async execute(code: string, browser: boolean = false) {
 		const url = 'https://github.com/login/oauth/access_token';
-		const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
-			params: {
-				client_id: process.env.GITHUB_CLIENT_ID,
-				client_secret: process.env.GITHUB_CLIENT_SECRET,
-				code,
-			},
-			headers: {
-				"Accept": "application/json",
-			}
-		});
 
-		const response = await axios.get<UserResponse>('https://api.github.com/user', {
-			headers: {
-				authorization: `Bearer ${accessTokenResponse.access_token}`
-			}
-		});
-
-		const { login, id, avatar_url, name } = response.data;
-
-		let user = await prismaClient.user.findFirst({
-			where: {
-				github_id: id,
-			}
-		});
-
-		if (!user) {
-			user = await prismaClient.user.create({
-				data: {
-					github_id: id,
-					login,
-					avatar_url,
-					name,
+		const client_secret = browser ? process.env.GITHUB_CLIENT_SECRET_BROWSER : process.env.GITHUB_CLIENT_SECRET;
+		const client_id = browser ? process.env.GITHUB_CLIENT_ID_BROWSER : process.env.GITHUB_CLIENT_ID;
+	
+		try {
+			const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
+				params: {
+					client_id,
+					client_secret,
+					code,
+				},
+				headers: {
+					"Accept": "application/json",
 				}
-			})
-		}
-
-		const tokenUser = {
-			user: {
-				name: user.name,
-				avatar_url: user.avatar_url,
-				id: user.id
-			},
-		}
-
-		const token = sign(
-			tokenUser, process.env.JWT_SECRET,
-			{
-				subject: user.id,
-				expiresIn: '1d'
+			});
+	
+			const response = await axios.get<UserResponse>('https://api.github.com/user', {
+				headers: {
+					authorization: `Bearer ${accessTokenResponse.access_token}`
+				}
+			});
+	
+			const { login, id, avatar_url, name } = response.data;
+	
+			let user = await prismaClient.user.findFirst({
+				where: {
+					github_id: id,
+				}
+			});
+	
+			if (!user) {
+				user = await prismaClient.user.create({
+					data: {
+						github_id: id,
+						login,
+						avatar_url,
+						name,
+					}
+				})
 			}
-		);
-
-		return { token, user };
+	
+			const tokenUser = {
+				user: {
+					name: user.name,
+					avatar_url: user.avatar_url,
+					id: user.id
+				},
+			}
+	
+			const token = sign(
+				tokenUser, process.env.JWT_SECRET,
+				{
+					subject: user.id,
+					expiresIn: '1d'
+				}
+			);
+	
+			return { token, user };
+		} catch (err) {
+			console.log(err)
+			throw err;
+		}
 	}
 }
 
